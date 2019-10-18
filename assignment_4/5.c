596664d2a1414ef5d6070541c1108d75fdcb8e24
abhishek-pickachu <56737844+abhishek-pickachu@users.noreply.github.com>
2019-10-19 00:48:16 +0530

    Update 5.c

--- assignment_4/5.c
+++ assignment_4/5.c
@@ -6,7 +6,7 @@ Marks in Chemistry >=60,
 Total in all three subjects >=190 or Total in Mathematics and Chemistry >=130 */
 
 #include <stdio.h>
-int main() {
+void main() {
 int math, phy, chem, total, tot;
 printf("Enter the marks obtained in Maths, Physics, Chemistry : ");
 scanf("%d%d%d",&math,&phy,&chem);
@@ -16,5 +16,4 @@ if((math>=65 && phy>=55 && chem>=60 && total>=190) || tot>=130)
 	printf("The candidate is eligible for admission.\n");
 else
 	printf("The candidate is not eligible for admission.\n");
-return 0;
 }
